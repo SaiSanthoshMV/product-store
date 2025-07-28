004f7b127877288767389f6788abbf4b3b7b4c3a
SaiSanthoshMV <mvs.sai999@gmail.com>
2025-07-29 00:17:04 +0530

    used routes models and controllers in backend app maintainability

---
+++ backend/controller/product.controller.js
@@ -0,0 +1,60 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+
+export const getProduct = async (req,res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products})
+    }catch(error){
+        res.status(404).json({success: false, message: "surver error"})
+        console.log("error in fetching data: ", error.message)
+    }
+};
+
+export const createProduct = async (req, res) => {
+    // res.send("Server is ready");
+    const product = req.body;
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success: false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product);
+
+    try{
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct})
+    }catch(error){
+        console.log("error in saving product: ", error);
+        res.status(500).json({ success: false, message: "internal server error"});
+    }
+};
+
+export const updateProduct = async (req, res) =>{
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "invalid product Id"})
+    }
+
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct})
+    }catch(error){
+        res.status(500).json({success: false, message: "Server error"});
+        console.log("error in updating product", error)
+    }
+};
+
+export const deleteProduct = async (req, res) =>{
+    const {id} = req.params;
+
+    try{
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "Product Deleted"});
+    }catch(error){
+        console.log("error in deleting product", error)
+        res.status(404).json({success: false, message: "Product Not Found!"});
+    }
+};
